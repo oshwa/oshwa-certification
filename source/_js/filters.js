9aa48d69735234bb96b3cd8bac5e3579e1352df3
enabek <enabek@gmail.com>
2018-07-16 19:44:52 -0400

    Add list page and filter dropdown

---
+++ source/_js/filters.js
@@ -0,0 +1,20 @@
+const Filters = {
+  openFilters: () => {
+    $('.open-filters').click(() => {
+      $('.filter-container').toggleClass('js-active');
+      $('.expand').toggleClass('expanded');
+
+      if ($('.expand').hasClass('expanded')) {
+        $('.expand').html('expand_less');
+      } else {
+        $('.expand').html('expand_more');
+      }
+
+    });
+  },
+  init() {
+    this.openFilters();
+  }
+}
+
+module.exports = Filters;
