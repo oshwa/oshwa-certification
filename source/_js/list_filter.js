12ab2839026f8cbe956ea3a6612d8dbc42965113
enabek <enabek@gmail.com>
2018-07-21 20:37:08 -0400

    Filter list by search on keyup and dropdown progress

--- source/_js/list_filter.js
+++ source/_js/list_filter.js
@@ -9,6 +9,9 @@ const ListFilter = {
       'project__hardware_license',
       'project__documentation_license',
       'project__software_license'
+      // { name: 'project__hardware_license', attr: 'data-target'},
+      // { name: 'project__documentation_license', attr: 'data-target'},
+      // { name: 'project__software_license', attr: 'data-target'}
     ],
     fuzzySearch: {
       searchClass: 'fuzzy-search',
@@ -18,29 +21,40 @@ const ListFilter = {
       multiSearch: true
     }
   },
-  // projectList: new List('project_data', ListFilter.options),
+  projectList: undefined,
+  searchString: '',
   createList: () => {
-    const projectList = new List('project_data', ListFilter.options);
+    ListFilter.projectList = new List('project_data', ListFilter.options);
+
     if (window.location.href.includes('list')) {
-      projectList.sort('project__name', { order: 'asc' });
+      ListFilter.projectList.sort('project__name', { order: 'asc' });
     }
   },
-  filterByCheckboxes: () => {
-    $('body').on('change', $('input[type="checkbox"]:checked'), () => {
-
+  filterBySearch: () => {
+    $('#searchfield').on('keyup', e => {
+      ListFilter.searchString = $(e.currentTarget).val();
+      ListFilter.projectList.search(ListFilter.searchString);
     });
   },
-  // filterByDropdowns: () => {
-  //
-  // },
-  // filterBySearch: () => {
-  //
+  filterByDropdowns: () => {
+    $('.dropdown').on('change', e => {
+      const id = e.currentTarget.id;
+      const value = $(e.currentTarget).val();
+
+      id ? ListFilter.projectList.search(value) : false;
+    });
+  },
+  // filterByCheckboxes: () => {
+  //   $('body').on('change', $('input[type="checkbox"]:checked'), () => {
+  //     console.log(ListFilter.allCheckboxes)
+  //     // ListFilter.allCheckboxes.push($('input[type="checkbox"]:checked').val())
+  //   });
   // },
   init() {
     this.createList();
-    this.filterByCheckboxes();
-    // this.filterByDropdowns();
-    // this.filterBySearch();
+    this.filterBySearch();
+    this.filterByDropdowns();
+    // this.filterByCheckboxes();
   }
 };
 
