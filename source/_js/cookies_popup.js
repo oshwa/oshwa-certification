6c903e02a02f75374ab105bbf7d0e380035762f2
enabek <enabek@gmail.com>
2018-09-10 12:23:51 -0400

    Remove disagree button and cookie clearing script

--- source/_js/cookies_popup.js
+++ source/_js/cookies_popup.js
@@ -14,33 +14,18 @@ const CookiesPopup = {
         CookiesPopup.cookies.set('user_cookie_agreement', 'agree', { expires: 365 });
       } else if (clickedValue === 'disagree') {
         CookiesPopup.cookies.set('user_cookie_agreement', 'disagree', { expires: 365 });
-        CookiesPopup.clearCookies();
       }
       $('.cookies-popup').animate({ bottom: '-100px' }, 'slow');
     });
-  },
-  clearCookies: () => {
-    const allCookies = CookiesPopup.cookies.all();
-    for (const cookie in allCookies) {
-      if (allCookies.hasOwnProperty(cookie) && cookie !== 'user_cookie_agreement') {
-        CookiesPopup.cookies.erase(cookie);
-      }
-    }
   },
   hidePopupBasedOnCookie: () => {
     if (CookiesPopup.cookies.get('user_cookie_agreement')) {
       $('.cookies-popup').addClass('hidden');
     }
   },
-  clearCookiesOnLoad: () => {
-    if (CookiesPopup.cookies.get('user_cookie_agreement') === 'disagree') {
-      CookiesPopup.clearCookies();
-    }
-  },
   init() {
     this.slideUp();
     this.slideDown();
-    this.clearCookiesOnLoad();
     this.hidePopupBasedOnCookie();
   }
 };
