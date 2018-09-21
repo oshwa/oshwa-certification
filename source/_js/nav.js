2b5716df8218643373f59e9e80c6e3fc5cbd223e
enabek <enabek@gmail.com>
2018-09-21 13:16:37 -0400

    Add footer text and re-arrange exemplar licenses and fix nav menu and list menu on mobile overlap

--- source/_js/nav.js
+++ source/_js/nav.js
@@ -1,15 +1,15 @@
 const Nav = {
   openMenu: () => {
-    $('.js-trigger-menu').on('click', () => {
+    $('.menu-button').on('click', () => {
       $('.js-target-menu').toggleClass('active-menu');
-      $('.top-bar__button').toggleClass('active-menu-button');
+      $('.menu-button').toggleClass('active-menu-button');
       $('#overlay').toggleClass('js-active');
     });
   },
   closeMenu: () => {
     $('.close-menu').on('click', () => {
       $('.js-target-menu').removeClass('active-menu');
-      $('.top-bar__button').removeClass('active-menu-button');
+      $('.menu-button').removeClass('active-menu-button');
       $('#overlay').removeClass('js-active');
     });
 
