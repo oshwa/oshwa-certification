cd1e47b97608b01933c7eb93d989efb646252d75
Denny Tek <tek.denny@gmail.com>
2018-09-26 15:46:05 -0400

    Add circleci

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -5,13 +5,22 @@ var ghPages = require('gulp-gh-pages'),
 
 /////////////////////////// DEPLOY TASKS ///////////////////////////
 module.exports = function() {
+
+  gulp.task('circleci', function() {
+    return gulp.src('.circleci/config.yml').pipe(gulp.dest('_site/.circleci/'));
+  });
+
+
   gulp.task('push-gh-master', shell.task(['git push origin master']));
 
   gulp.task('push-gh-pages', function() {
-    return gulp.src('_site/**/*').pipe(ghPages({ force: true }));
+    return (gulp
+        .src('_site/**/*', { dot: true })
+        // .src(['_site/**/*', '_site/.**/*' ])
+        .pipe(ghPages({ force: true })) );
   });
 
   gulp.task('deploy', function(callback) {
-    runSequence('build:prod', 'push-gh-master', 'push-gh-pages', callback);
+    runSequence('build:prod', 'circleci', 'push-gh-master', 'push-gh-pages', callback);
   });
 };
