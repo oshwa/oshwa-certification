ef67b59aee137546c6ddc34b10cec7d79b3fe863
Denny Tek <denny@objectively.is>
2024-07-15 12:26:37 -0400

    Move from circleci to github actions

--- gulpfile.js
+++ gulpfile.js
@@ -121,7 +121,7 @@ gulp.task('push-gh-main', shell.task(['git push origin main']));
 
 gulp.task('push-gh-pages', () => gulp.src('_site/**/*', { dot: true }).pipe(ghPages({ force: true })));
 
-gulp.task('deploy', gulp.series('build:prod', 'circleci', 'push-gh-main', 'push-gh-pages'));
+gulp.task('build', gulp.series('build:prod'));
 
 /* =========================================
   serve
